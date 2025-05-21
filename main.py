035f5c68426e39017046070c1599714f5873b53b
Anthony DeMatteo <anthde13@gmail.com>
2025-05-21 17:21:21 -0400

    basic query

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import os
 import requests
+from dotenv import load_dotenv
 
-api_key = os.environ.get("USDA_API_KEY")
+load_dotenv()
 
+API_KEY = os.environ.get("USDA_API_KEY")
+SEARCH_TERM = "chicken breast"
+
+url = "https://api.nal.usda.gov/fdc/v1/foods/search"
+params = {
+    "query": SEARCH_TERM,
+    "pageSize": 3
+}
 headers = {
-    'X-Api-Key': api_key
+    'X-Api-Key': API_KEY
 }
 
-response = requests.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1', headers=headers)
+response = requests.get(url, params=params, headers=headers)
 
-print(response.status_code)
+if response.status_code == 200:
+    data = response.json()
+    print("Search results for:", SEARCH_TERM)
+    for food in data.get("foods", []): # if "foods" missing, return empty list
+        print(f"- {food.get('description')} (FDC ID: {food.get('fdcId')})")
+else:
+    print("Error:", response.status_code, response.text)
+
+# response = requests.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1', headers=headers)
+
+
+
